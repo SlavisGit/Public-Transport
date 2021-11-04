54faca17ca68e9747ce8cbd2a2539135c39d75b4
Alyona <a.nazarova521@gmail.com>
2021-11-04 18:46:46 +0200

    Create start page

--- Project/BusServices/src/main/java/module-info.java
+++ Project/BusServices/src/main/java/module-info.java
@@ -10,12 +10,11 @@ module tuvarna.sit.busservices {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
 
-    opens tuvarna.sit.busservices to javafx.fxml;
-    exports tuvarna.sit.busservices;
+
     exports tuvarna.sit.busservices.application;
     opens tuvarna.sit.busservices.application to javafx.fxml;
-    exports tuvarna.sit.busservices.presentation;
-    opens tuvarna.sit.busservices.presentation to javafx.fxml;
+
+
     exports tuvarna.sit.busservices.presentation.controllers;
     opens tuvarna.sit.busservices.presentation.controllers to javafx.fxml;
 }
--- Project/BusServices/src/main/java/tuvarna/sit/busservices/presentation/controllers/HelloController.java
+++ Project/BusServices/src/main/java/tuvarna/sit/busservices/presentation/controllers/HelloController.java
@@ -1,14 +1,38 @@
 package tuvarna.sit.busservices.presentation.controllers;
 
+import java.net.URL;
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="ID_DLG_LOG_IN"
+    private AnchorPane ID_DLG_LOG_IN; // Value injected by FXMLLoader
+
+    @FXML // fx:id="ID_BTN_ADMIN"
+    private Button ID_BTN_ADMIN; // Value injected by FXMLLoader
+
+    @FXML // fx:id="ID_BTN_CLIENT"
+    private Button ID_BTN_CLIENT; // Value injected by FXMLLoader
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert ID_DLG_LOG_IN != null : "fx:id=\"ID_DLG_LOG_IN\" was not injected: check your FXML file 'Untitled'.";
+        assert ID_BTN_ADMIN != null : "fx:id=\"ID_BTN_ADMIN\" was not injected: check your FXML file 'Untitled'.";
+        assert ID_BTN_CLIENT != null : "fx:id=\"ID_BTN_CLIENT\" was not injected: check your FXML file 'Untitled'.";
+
+
     }
 }
+
